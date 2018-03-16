e89ea6dbe9e348333f1bf426634a5bdc4b017776
Jonas Jaeger <jonas.jaeger@et.hs-fulda.de>
2018-03-16 13:56:01 +0100

    Added test script

---
+++ experiments/test.sh
@@ -0,0 +1,32 @@
+##################
+#---Experiment1---
+##################
+GRAPH="/home/thomas/cnn_feature_extractor/tf_inception/classify_image_graph_def.pb"
+FOLDER_ROOT="/home/thomas/experiments/dataset"
+
+FOLDER_IMAGES="/images"
+FOLDER_GROUND_TRUTH="/ground-truth"
+EVAL_TRAIN="quicktest/train.json"
+EVAL_TEST="quicktest/test.json"
+EVAL_VALIDATE="/validate.json"
+DEPTH_MIN=0.0
+DEPTH_MAX=10.0
+PATCH_SIZE=120
+
+
+pudb3 ../main.py \
+--pattern SP_SLIC \
+--features cnn \
+--graph $GRAPH \
+--mode dontcare \
+--folder_root $FOLDER_ROOT \
+--folder_images $FOLDER_IMAGES \
+--folder_ground_truth $FOLDER_GROUND_TRUTH \
+--eval_test $EVAL_TEST \
+--eval_train $EVAL_TRAIN \
+--eval_validate $EVAL_VALIDATE \
+--patch_size_width $PATCH_SIZE \
+--patch_size_height $PATCH_SIZE \
+--output "/home/thomas/experiments/results/exp1-cnn-sp-slic.json" \
+--depth_min $DEPTH_MIN \
+--depth_max $DEPTH_MAX \
