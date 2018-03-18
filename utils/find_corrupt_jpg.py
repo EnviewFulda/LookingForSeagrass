875c6f7000510d7b0dd9c9fbc3d978e98b50a96d
Jonas Jaeger <jonas.jaeger@et.hs-fulda.de>
2018-03-18 13:42:49 +0100

    Added find_corrupt_jpg.py

---
+++ utils/find_corrupt_jpg.py
@@ -0,0 +1,14 @@
+import cv2
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--imgdir", help="Path to folder with images to check")
+args = parser.parse_args()
+
+for root, dirnames, filenames in os.walk(args.imgdir):
+    for filename in filenames:
+        if filename.endswith('.jpg'):
+            img_path = os.path.join(root, filename)
+            #print(img_path)
+            img = cv2.imread(img_path)
