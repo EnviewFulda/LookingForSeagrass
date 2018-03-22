71968b00ac5db11cb27ff901a254c73cf9333d56
Jonas Jaeger <jonas.jaeger@et.hs-fulda.de>
2018-03-22 22:27:47 +0100

    Added experiments

--- experiments/exp2.sh
+++ experiments/exp2.sh
@@ -1,5 +1,5 @@
 ##################
-#---Experiment1---
+#---Experiment2---
 ##################
 GRAPH="/home/jaeger/papers/2018-oceans-seagrass/classify_image_graph_def.pb"
 FOLDER_ROOT="/home/jaeger/papers/2018-oceans-seagrass/dataset"
@@ -10,11 +10,11 @@ EVAL_TRAIN="/train.json"
 EVAL_TEST="/test.json"
 EVAL_VALIDATE="/validate.json"
 DEPTH_MIN=0.0
-DEPTH_MAX=10.0
-PATCH_SIZE=240
+DEPTH_MAX=2.0
 
 MAIN_PROGRAM="LookingForSeagrass/main.py"
 
+PATCH_SIZE=240
 python3 $MAIN_PROGRAM \
 --pattern SP_SLIC \
 --features cnn \
@@ -28,44 +28,9 @@ python3 $MAIN_PROGRAM \
 --eval_validate $EVAL_VALIDATE \
 --patch_size_width $PATCH_SIZE \
 --patch_size_height $PATCH_SIZE \
---output "/home/jaeger/papers/2018-oceans-seagrass/results/exp1-cnn-sp-slic.json" \
+--output "/home/jaeger/papers/2018-oceans-seagrass/results/exp2-cnn-sp-slic-240-0-2.json" \
 --depth_min $DEPTH_MIN \
 --depth_max $DEPTH_MAX \
-
-python3 $MAIN_PROGRAM \
---pattern SP_SLIC \
---features hog \
---graph $GRAPH \
---mode dontcare \
---folder_root $FOLDER_ROOT \
---folder_images $FOLDER_IMAGES \
---folder_ground_truth $FOLDER_GROUND_TRUTH \
---eval_test $EVAL_TEST \
---eval_train $EVAL_TRAIN \
---eval_validate $EVAL_VALIDATE \
---patch_size_width $PATCH_SIZE \
---patch_size_height $PATCH_SIZE \
---output "/home/jaeger/papers/2018-oceans-seagrass/results/exp1-hog-sp-slic.json" \
---depth_min $DEPTH_MIN \
---depth_max $DEPTH_MAX \
-
-python3 $MAIN_PROGRAM \
---pattern SP_SLIC \
---features lbp \
---graph $GRAPH \
---mode dontcare \
---folder_root $FOLDER_ROOT \
---folder_images $FOLDER_IMAGES \
---folder_ground_truth $FOLDER_GROUND_TRUTH \
---eval_test $EVAL_TEST \
---eval_train $EVAL_TRAIN \
---eval_validate $EVAL_VALIDATE \
---patch_size_width $PATCH_SIZE \
---patch_size_height $PATCH_SIZE \
---output "/home/jaeger/papers/2018-oceans-seagrass/results/exp1-lbp-sp-slic.json" \
---depth_min $DEPTH_MIN \
---depth_max $DEPTH_MAX \
-
 
 python3 $MAIN_PROGRAM \
 --pattern RP \
@@ -80,13 +45,31 @@ python3 $MAIN_PROGRAM \
 --eval_validate $EVAL_VALIDATE \
 --patch_size_width $PATCH_SIZE \
 --patch_size_height $PATCH_SIZE \
---output "/home/jaeger/papers/2018-oceans-seagrass/results/exp1-cnn-rp.json" \
+--output "/home/jaeger/papers/2018-oceans-seagrass/results/exp2-cnn-rp-240-0-2.json" \
+--depth_min $DEPTH_MIN \
+--depth_max $DEPTH_MAX \
+
+PATCH_SIZE=180
+python3 $MAIN_PROGRAM \
+--pattern SP_SLIC \
+--features cnn \
+--graph $GRAPH \
+--mode dontcare \
+--folder_root $FOLDER_ROOT \
+--folder_images $FOLDER_IMAGES \
+--folder_ground_truth $FOLDER_GROUND_TRUTH \
+--eval_test $EVAL_TEST \
+--eval_train $EVAL_TRAIN \
+--eval_validate $EVAL_VALIDATE \
+--patch_size_width $PATCH_SIZE \
+--patch_size_height $PATCH_SIZE \
+--output "/home/jaeger/papers/2018-oceans-seagrass/results/exp2-cnn-sp-slic-180-0-2.json" \
 --depth_min $DEPTH_MIN \
 --depth_max $DEPTH_MAX \
 
 python3 $MAIN_PROGRAM \
 --pattern RP \
---features hog \
+--features cnn \
 --graph $GRAPH \
 --mode dontcare \
 --folder_root $FOLDER_ROOT \
@@ -97,13 +80,31 @@ python3 $MAIN_PROGRAM \
 --eval_validate $EVAL_VALIDATE \
 --patch_size_width $PATCH_SIZE \
 --patch_size_height $PATCH_SIZE \
---output "/home/jaeger/papers/2018-oceans-seagrass/results/exp1-hog-rp.json" \
+--output "/home/jaeger/papers/2018-oceans-seagrass/results/exp2-cnn-rp-180-0-2.json" \
+--depth_min $DEPTH_MIN \
+--depth_max $DEPTH_MAX \
+
+PATCH_SIZE=120
+python3 $MAIN_PROGRAM \
+--pattern SP_SLIC \
+--features cnn \
+--graph $GRAPH \
+--mode dontcare \
+--folder_root $FOLDER_ROOT \
+--folder_images $FOLDER_IMAGES \
+--folder_ground_truth $FOLDER_GROUND_TRUTH \
+--eval_test $EVAL_TEST \
+--eval_train $EVAL_TRAIN \
+--eval_validate $EVAL_VALIDATE \
+--patch_size_width $PATCH_SIZE \
+--patch_size_height $PATCH_SIZE \
+--output "/home/jaeger/papers/2018-oceans-seagrass/results/exp2-cnn-sp-slic-120-0-2.json" \
 --depth_min $DEPTH_MIN \
 --depth_max $DEPTH_MAX \
 
 python3 $MAIN_PROGRAM \
 --pattern RP \
---features lbp \
+--features cnn \
 --graph $GRAPH \
 --mode dontcare \
 --folder_root $FOLDER_ROOT \
@@ -114,50 +115,11 @@ python3 $MAIN_PROGRAM \
 --eval_validate $EVAL_VALIDATE \
 --patch_size_width $PATCH_SIZE \
 --patch_size_height $PATCH_SIZE \
---output "/home/jaeger/papers/2018-oceans-seagrass/results/exp1-lbp-rp.json" \
+--output "/home/jaeger/papers/2018-oceans-seagrass/results/exp2-cnn-rp-120-0-2.json" \
 --depth_min $DEPTH_MIN \
 --depth_max $DEPTH_MAX \
 
-
-##################
-
-python3 $MAIN_PROGRAM \
---pattern SP_CW \
---features hog \
---graph $GRAPH \
---mode dontcare \
---folder_root $FOLDER_ROOT \
---folder_images $FOLDER_IMAGES \
---folder_ground_truth $FOLDER_GROUND_TRUTH \
---eval_test $EVAL_TEST \
---eval_train $EVAL_TRAIN \
---eval_validate $EVAL_VALIDATE \
---patch_size_width $PATCH_SIZE \
---patch_size_height $PATCH_SIZE \
---output "/home/jaeger/papers/2018-oceans-seagrass/results/exp1-hog-sp-cw.json" \
---depth_min $DEPTH_MIN \
---depth_max $DEPTH_MAX \
-
-##################
-
-python3 $MAIN_PROGRAM \
---pattern SP_CW \
---features lbp \
---graph $GRAPH \
---mode dontcare \
---folder_root $FOLDER_ROOT \
---folder_images $FOLDER_IMAGES \
---folder_ground_truth $FOLDER_GROUND_TRUTH \
---eval_test $EVAL_TEST \
---eval_train $EVAL_TRAIN \
---eval_validate $EVAL_VALIDATE \
---patch_size_width $PATCH_SIZE \
---patch_size_height $PATCH_SIZE \
---output "/home/jaeger/papers/2018-oceans-seagrass/results/exp1-lbp-sp-cw.json" \
---depth_min $DEPTH_MIN \
---depth_max $DEPTH_MAX \
-
-
+PATCH_SIZE=240
 python3 $MAIN_PROGRAM \
 --pattern SP_CW \
 --features cnn \
@@ -171,8 +133,43 @@ python3 $MAIN_PROGRAM \
 --eval_validate $EVAL_VALIDATE \
 --patch_size_width $PATCH_SIZE \
 --patch_size_height $PATCH_SIZE \
---output "/home/jaeger/papers/2018-oceans-seagrass/results/exp1-cnn-sp-cw.json" \
+--output "/home/jaeger/papers/2018-oceans-seagrass/results/exp2-cnn-sp-cw-240-0-2.json" \
 --depth_min $DEPTH_MIN \
 --depth_max $DEPTH_MAX \
 
+PATCH_SIZE=180
+python3 $MAIN_PROGRAM \
+--pattern SP_CW \
+--features cnn \
+--graph $GRAPH \
+--mode dontcare \
+--folder_root $FOLDER_ROOT \
+--folder_images $FOLDER_IMAGES \
+--folder_ground_truth $FOLDER_GROUND_TRUTH \
+--eval_test $EVAL_TEST \
+--eval_train $EVAL_TRAIN \
+--eval_validate $EVAL_VALIDATE \
+--patch_size_width $PATCH_SIZE \
+--patch_size_height $PATCH_SIZE \
+--output "/home/jaeger/papers/2018-oceans-seagrass/results/exp2-cnn-sp-cw-180-0-2.json" \
+--depth_min $DEPTH_MIN \
+--depth_max $DEPTH_MAX \
+
+PATCH_SIZE=120
+python3 $MAIN_PROGRAM \
+--pattern SP_CW \
+--features cnn \
+--graph $GRAPH \
+--mode dontcare \
+--folder_root $FOLDER_ROOT \
+--folder_images $FOLDER_IMAGES \
+--folder_ground_truth $FOLDER_GROUND_TRUTH \
+--eval_test $EVAL_TEST \
+--eval_train $EVAL_TRAIN \
+--eval_validate $EVAL_VALIDATE \
+--patch_size_width $PATCH_SIZE \
+--patch_size_height $PATCH_SIZE \
+--output "/home/jaeger/papers/2018-oceans-seagrass/results/exp2-cnn-sp-cw-120-0-2.json" \
+--depth_min $DEPTH_MIN \
+--depth_max $DEPTH_MAX \
 
